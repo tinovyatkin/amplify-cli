072ae66e554276910ad5fd8f2721d9746206522c
Sachin Panemangalore <sachinrp@amazon.com>
2021-08-17 00:21:39 -0700

    added dynamodb and s3 schema generation calls

--- packages/amplify-category-storage/resources/genInputSchema.ts
+++ packages/amplify-category-storage/resources/genInputSchema.ts
@@ -1,18 +1,22 @@
-import { TypeDef, CLIInputSchemaGenerator} from "../../amplify-cli-core/lib";
+import {TypeDef, CLIInputSchemaGenerator} from "amplify-cli-core";
+
+//ResourceProvider TypeDefs
+const DDBStorageTypeDef : TypeDef = {
+  typeName: 'DDBCLIWalkthroughInterfaces',
+  service: 's3',
+  relativeSourcePaths: ['index.ts'],
+}
+const S3StorageTypeDef : TypeDef = {
+  typeName: 'S3CLIWalkthroughInterfaces',
+  service: 's3',
+  relativeSourcePaths: ['index.ts']
+}
 
 // Defines the type names and the paths to the TS files that define them
-const storageTypeDefs: TypeDef[] = [
-  {
-    typeName: 'S3CLIWalkthroughInterfaces',
-    service: 's3',
-    relativeSourcePaths: ['index.ts'],
-  },
-  {
-    typeName: 'DDBCLIWalkthroughInterfaces',
-    service: 's3',
-    relativeSourcePaths: ['index.ts'],
-  },
+const storageCategoryTypeDefs: TypeDef[] = [
+  DDBStorageTypeDef,
+  S3StorageTypeDef
 ];
 
-const schemaGenerator = new CLIInputSchemaGenerator(S3TypeDefs);
+const schemaGenerator = new CLIInputSchemaGenerator(storageCategoryTypeDefs);
 schemaGenerator.generateJSONSchemas(); //convert CLI input data into json schemas.
